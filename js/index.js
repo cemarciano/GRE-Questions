2ef7d4d2b6fb1ae2fa1321e4e650fdec36ddbdee
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2019-07-28 01:53:34 -0300

    Add GRE Guide and GRE Verbal Pratice

--- js/index.js
+++ js/index.js
@@ -6,11 +6,27 @@ $(document).ready(function(){
 
 	// Loads data:
 	jQuery.get({ url: 'data.json', cache: false }).done(function (data) {
-		// Creates a progress bar:
-		data[0]["manhattan"].forEach(section => {
-			console.log(section.completed/section.total)
-			$("#all-progress-bars").append( createProgressBar(section.name, (section.completed/section.total)*100+"%", section.total*horizontalScale+"px") );
-		});
+
+		const books = [
+			{"name": "manhattan", "title": "GRE Manhattan 5lb. Book"},
+			{"name": "gre-guide", "title": "GRE Official Guide"},
+			{"name": "gre-verbal-practice", "title": "GRE Verbal Practice"}
+		]
+
+		books.forEach(book => {
+			let div = $("<div></div>").addClass("book");
+			// Creates title:
+			div.append(
+				$("<h1></h1>").text(book.title)
+			);
+			// Creates progress bars:
+			data[0][book.name].forEach(section => {
+				div.append( createProgressBar(section.name, (section.completed/section.total)*100+"%", section.total*horizontalScale+"px") );
+			});
+			// Adds to the DOM:
+			$("body").append( div );
+		})
+
 	});
 
 });
